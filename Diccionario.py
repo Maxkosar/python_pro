8e5a385e759cf2b85b9bffc1131f2e64adb30c28
Maxkosar <kosarmaximo8@gmail.com>
2026-06-17 20:21:19 -0300

    Create Diccionario.py

---
+++ Diccionario.py
@@ -0,0 +1,17 @@
+meme_dict = {
+            "CRINGE": "Algo excepcionalmente raro o embarazoso",
+            "LOL": "Una respuesta común a algo gracioso",
+            "ROFL": "Una respuesta a una broma",
+            "SHEESH":"ligera desaprobación",
+            "CREEPY":"aterrador, siniestro",
+            "AGGRO":"Ponerse agresivo/enojado"
+            }
+
+
+word = input("Escribe una palabra que no entiendas (¡con mayúsculas!): ")
+
+if word in meme_dict.keys():
+    print(meme_dict[word])
+else:
+    print("Tu palabra no esta en el diciionario")
+    # ¿Qué hacer si no se encuentra la palabra?
